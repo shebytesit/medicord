641ba40bd2bd20c878f46dc9c001504675e97b94
liu351 <liu351@purdue.edu>
2013-09-13 21:23:57 -0400

    fix createAccount

--- MedicalDatabase/src/medicaldatabase/Importdb.java
+++ MedicalDatabase/src/medicaldatabase/Importdb.java
@@ -25,7 +25,7 @@ public class Importdb {
                 //stat.execute(drop1);
                 //stat.execute(drop2);
                 //stat.execute(drop3);
-                String createAccount = "CREATE TABLE ACCOUNT (username VARCHAR(100), password VARCHAR(100), id varchar2(20),primary key(username), unique(id))";
+                String createAccount = "CREATE TABLE ACCOUNT (username VARCHAR(100), password VARCHAR(100) not null, id varchar2(20),primary key(username), unique(id))";
                 String createPatient = "create table PATIENT (pid varchar2(20), name varchar2(20), dob varchar2(20), gender VARCHAR(100), allergies VARCHAR(100), medication VARCHAR(100), history VARCHAR(100), econtact VARCHAR(100), primary key (pid), foreign key (pid) references ACCOUNT)";
                 String createDoctor = "create table DOCTOR (did varchar2(20), name varchar2(20),gender varchar2(20),hospital varchar2(20),specialization varchar2(20), primary key (did),foreign key (did) references ACCOUNT )";
                 String createAppointment = "CREATE TABLE APPOINTMENT (aid varchar2(20), dates varchar2(20), did varchar2(20), pid varchar2(20),reason VARCHAR(100), foreign key (did) references DOCTOR, foreign key (pid) references PATIENT, primary key (aid))";
@@ -127,15 +127,16 @@ public class Importdb {
 			System.out.println(patientTest);
 			stmtST.execute(patientTest);
 			if (type.equals("patient")) {
-				id = "p" + id;
 				patientTest = "INSERT INTO  PATIENT" + "(pid)" + " VALUES( '"
 						+ id + "')";
+                        System.out.println(patientTest);
+
 			}
 			if (type.equals("doctor")) {
-				id = "d" + id;
 				patientTest = "INSERT INTO  DOCTOR" + "(did)"
 
 				+ " VALUES( '" + id + "')";
+			System.out.println(patientTest);
 
 			}
 
