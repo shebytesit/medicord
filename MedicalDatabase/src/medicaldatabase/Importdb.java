13d93ce208ac8c19cbeb2a0da27f31d8409ea353
Seth Newman <>
2013-09-13 21:53:49 -0400

    various changes to patient and such

--- MedicalDatabase/src/medicaldatabase/Importdb.java
+++ MedicalDatabase/src/medicaldatabase/Importdb.java
@@ -24,21 +24,10 @@ public class Importdb {
                 stat.execute(drop1);
                 stat.execute(drop2);
                 stat.execute(drop3);  */
-                String createAccount = "CREATE TABLE ACCOUNT (username VARCHAR(100), password VARCHAR(100), id varchar2(20),primary key(username), unique(id))";
+                String createAccount = "CREATE TABLE ACCOUNT (username VARCHAR(100), password VARCHAR(100) not null, id varchar2(20),primary key(username), unique(id))";
                 String createPatient = "create table PATIENT (pid varchar2(20), name varchar2(20), dob varchar2(20), gender VARCHAR(100), allergies VARCHAR(100), medication VARCHAR(100), history VARCHAR(100), econtact VARCHAR(100), primary key (pid))";
                 String createDoctor = "create table DOCTOR (did varchar2(20), name varchar2(20),gender varchar2(20),hospital varchar2(20),specialization varchar2(20), primary key (did) )";
                 String createAppointment = "CREATE TABLE APPOINTMENT (aid varchar2(20), dates varchar2(20), did varchar2(20), pid varchar2(20),reason VARCHAR(100), primary key (aid))";
-=======
-
-                //stat.execute(drop);
-                //stat.execute(drop1);
-                //stat.execute(drop2);
-                //stat.execute(drop3);
-                String createAccount = "CREATE TABLE ACCOUNT (username VARCHAR(100), password VARCHAR(100) not null, id varchar2(20),primary key(username), unique(id))";
-                String createPatient = "create table PATIENT (pid varchar2(20), name varchar2(20), dob varchar2(20), gender VARCHAR(100), allergies VARCHAR(100), medication VARCHAR(100), history VARCHAR(100), econtact VARCHAR(100), primary key (pid))";
-                String createDoctor = "create table DOCTOR (did varchar2(20), name varchar2(20),gender varchar2(20),hospital varchar2(20),specialization varchar2(20), primary key (did))";
-                String createAppointment = "CREATE TABLE APPOINTMENT (aid varchar2(20), dates varchar2(20), did varchar2(20), pid varchar2(20),reason VARCHAR(100), foreign key (did) references DOCTOR, foreign key (pid) references PATIENT, primary key (aid))";
->>>>>>> branch 'master' of https://github.com/galarnyk/cs408.git
 
                 System.out.println(createAppointment);
                 stat.execute(createAccount);
