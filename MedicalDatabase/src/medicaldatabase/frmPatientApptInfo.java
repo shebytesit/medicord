9dd1d3a9d0ac1cd96b4fed3ab16a105090ed209a
Mark <>
2013-09-14 16:56:55 -0400

    Display doctors in combo box

--- MedicalDatabase/src/medicaldatabase/frmPatientApptInfo.java
+++ MedicalDatabase/src/medicaldatabase/frmPatientApptInfo.java
@@ -4,6 +4,10 @@
  */
 package medicaldatabase;
 
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
 /**
  *
  * @author Austin
@@ -13,8 +17,41 @@ public class frmPatientApptInfo extends javax.swing.JFrame {
     /**
      * Creates new form frmPatientApptInfo
      */
-    public frmPatientApptInfo() {
+    frmPatient patient;
+    String uid;
+    ResultSet rs;
+    ArrayList<String> doctorNames = new ArrayList<String>();
+    ArrayList<String> doctorUids = new ArrayList<String>();
+    public frmPatientApptInfo(frmPatient patient, String uid) {
         initComponents();
+        this.patient = patient;
+        this.uid = uid;
+        //Get list of doctors from database and populate cmbDoctors
+        rs = Importdb.getListofDoctorNames(this.uid);
+        if(rs != null){
+            String doctorName;
+            String doctorUid;
+            try{
+                while(rs.next()){
+                    doctorName = rs.getString("name");
+                    doctorUid = rs.getString("did");
+                    if(doctorName != null && doctorUid != null){
+                        doctorNames.add(doctorName);
+                        doctorUids.add(doctorUid);
+                    }else{
+                        System.out.println("Either a doctor name or uid was null with did: " + doctorUid
+                                   + " or name: " + doctorName + ".");
+                    }
+                }
+                for(int i = 0; i < doctorNames.size(); i++){
+                    cmbDoctors.addItem(doctorNames.get(i));
+                }
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+        }else{
+            JOptionPane.showMessageDialog(this, "No doctors found in database.");
+        }
     }
 
     /**
@@ -27,15 +64,15 @@ public class frmPatientApptInfo extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        txtDate = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox();
+        cmbDoctors = new javax.swing.JComboBox();
         jLabel3 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        txtReason = new javax.swing.JTextArea();
+        btnSchedule = new javax.swing.JButton();
+        btnCancel = new javax.swing.JButton();
+        btnLookup = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -43,24 +80,22 @@ public class frmPatientApptInfo extends javax.swing.JFrame {
 
         jLabel2.setText("Doctor:");
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
         jLabel3.setText("Reason for Visit:");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        txtReason.setColumns(20);
+        txtReason.setRows(5);
+        jScrollPane1.setViewportView(txtReason);
 
-        jButton1.setText("Schedule!");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnSchedule.setText("Schedule!");
+        btnSchedule.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnScheduleActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Cancel");
+        btnCancel.setText("Cancel");
 
-        jButton3.setText("Look at info");
+        btnLookup.setText("Look at info");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -77,13 +112,13 @@ public class frmPatientApptInfo extends javax.swing.JFrame {
                             .addComponent(jLabel3))
                         .addGap(30, 30, 30)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(cmbDoctors, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtDate, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(btnSchedule, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnCancel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnLookup, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -92,30 +127,30 @@ public class frmPatientApptInfo extends javax.swing.JFrame {
                 .addGap(33, 33, 33)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtDate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(29, 29, 29)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3))
+                    .addComponent(cmbDoctors, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnLookup))
                 .addGap(28, 28, 28)
                 .addComponent(jLabel3)
                 .addGap(30, 30, 30)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1)
+                        .addComponent(btnSchedule)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton2)))
+                        .addComponent(btnCancel)))
                 .addContainerGap(27, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnScheduleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnScheduleActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnScheduleActionPerformed
 
     /**
      * @param args the command line arguments
@@ -143,24 +178,17 @@ public class frmPatientApptInfo extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(frmPatientApptInfo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new frmPatientApptInfo().setVisible(true);
-            }
-        });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JComboBox jComboBox1;
+    private javax.swing.JButton btnCancel;
+    private javax.swing.JButton btnLookup;
+    private javax.swing.JButton btnSchedule;
+    private javax.swing.JComboBox cmbDoctors;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField txtDate;
+    private javax.swing.JTextArea txtReason;
     // End of variables declaration//GEN-END:variables
 }
