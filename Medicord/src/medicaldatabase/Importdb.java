2dd92e53d5e90f77566fd017ded72567c0128188
Lillian Liu <liu351@purdue.edu>
2013-09-25 20:18:59 -0400

    commit everything

--- Medicord/src/medicaldatabase/Importdb.java
+++ Medicord/src/medicaldatabase/Importdb.java
@@ -24,7 +24,7 @@ public class Importdb {
                 stat.execute(drop1);
                 stat.execute(drop2);
                 stat.execute(drop3);  
-                String createAccount = "CREATE TABLE ACCOUNT (username VARCHAR(100), password VARCHAR(100) not null, id varchar2(20),primary key(username), unique(id))";
+                String createAccount = "CREATE TABLE ACCOUNT (username VARCHAR(100), password VARCHAR(100) not null, id varchar2(20),primary key(id))";
                 String createPatient = "create table PATIENT (pid varchar2(20), name varchar2(20), dob varchar2(20), gender VARCHAR(100), allergies VARCHAR(100), medication VARCHAR(100), history VARCHAR(100), econtact VARCHAR(100), primary key (pid))";
                 String createDoctor = "create table DOCTOR (did varchar2(20), name varchar2(20),gender varchar2(20),hospital varchar2(20),specialization varchar2(20), dPhone varchar2(20), primary key (did) )";
                 String createAppointment = "CREATE TABLE APPOINTMENT (aid varchar2(20), dates varchar2(20), did varchar2(20), pid varchar2(20),reason VARCHAR(100),notes VARCHAR(100), primary key (aid))";
